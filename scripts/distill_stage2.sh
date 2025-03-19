5613eda47ee2cd5fb7073797236ed941cfa5fade
HongyuanTao <146729238+Hongyuan-Tao@users.noreply.github.com>
2025-03-19 22:14:25 +0800

    Update distill_stage2.sh

--- scripts/distill_stage2.sh
+++ scripts/distill_stage2.sh
@@ -8,7 +8,7 @@ accelerate launch \
   --main_process_port  "11122"  \
     distill_mmMamba.py \
   --model_config distill_mmMamba \
-  --distill_stage1_config distill_stage2_mmMamba \
+  --distill_stage2_config distill_stage2_mmMamba \
   --checkpoint_dir ./checkpoints \
   --train_stage2  \
   --verbose \
